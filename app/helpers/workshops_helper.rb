84bb6db3a9b26612c01d07baa86f60675abd967d
trevormast <trevormast@gmail.com>
2016-09-28 16:26:52 -0700

    Adds helpers to format workshop date and time

--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -2,4 +2,12 @@ module WorkshopsHelper
   def spots_left(workshop)
     workshop.limit - workshop.users.count
   end
+
+  def date_format(datetime)
+    datetime.strftime("%b %d, %Y")
+  end
+
+  def time_format(date_time)
+    date_time.strftime("%l:%m %p")
+  end
 end
