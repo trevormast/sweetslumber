b82c5a8f6fda22fc899c6ed4ef6769a63819bfbc
trevormast <trevormast@gmail.com>
2016-09-29 20:51:18 -0700

    Adds baby's age to registered users table in workshop#show

--- app/helpers/workshops_helper.rb
+++ app/helpers/workshops_helper.rb
@@ -11,7 +11,11 @@ module WorkshopsHelper
     date_time.strftime("%l:%m %p")
   end
 
-  def user_questionaire(user)
-    Registration.find_by(user_id: user, workshop_id: @workshop.id).questionaire
+  def user_responses(user, workshop)
+    Registration.find_by(user_id: user, workshop_id: workshop.id).questionaire
+  end
+
+  def baby_age(user, workshop)
+    distance_of_time_in_words(workshop.time, user_responses(user, workshop).baby_dob)
   end
 end
