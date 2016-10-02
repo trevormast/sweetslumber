1a89f9526e9c355ef475082e754512d3baf38c5f
trevormast <trevormast@gmail.com>
2016-10-01 22:58:36 -0700

    Adds StaticPages Controller specs

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
+  describe 'GET home' do
+    it 'gets the home page' do
+      get :home
+      expect(response).to render_template('home')
+    end
+  end
 end
