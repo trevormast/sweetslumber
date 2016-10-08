08e9067cade3eb34d7cf40aa6c73d495b27c5064
trevormast <trevormast@gmail.com>
2016-10-07 22:17:39 -0700

    Adds user specs

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -4,6 +4,10 @@ FactoryGirl.define do
     address '123 New st. New City, NS 12345'
   end
 
+  factory :user do
+
+  end
+
   factory :workshop do
     location
     users
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:valid_attributes) {
+    {
+      email: 'new@user.com',
+      password: 'password',
+      password_confirmation: 'password'
+    }
+  }
+
+  let(:invalid_attributes) {
+    {
+      email: 'new@user.com',
+    }
+  }
+
+  describe 'new user' do
+    context 'with valid attributes' do
+      it 'creates a new user' do
+        expect(User.new(valid_attributes)).to be_valid
+      end
+
+      it 'saves a new user' do
+        @user = User.new(valid_attributes)
+        expect { @user.save }.to change(User, :count).by(1)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'creates an invalid user' do
+        expect(User.new(invalid_attributes)).to be_invalid
+      end
+
+      it 'does not save user' do
+        @user = User.new(invalid_attributes)
+        expect { @user.save }.not_to change(User, :count)
+      end
+    end
+  end
 end
