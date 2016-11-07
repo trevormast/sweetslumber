cdb9d34955896be55c94cbfaaa88253ca13d7a09
trevormast <trevormast@gmail.com>
2016-11-07 10:43:58 -0800

    Adds about* rake routes
    Addresses #13

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
-  get '/about', :to => 'static_pages#about'
+
+  get '/class', :to => 'static_pages#about_class'
+  get '/teacher', :to => 'static_pages#about_teacher'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
