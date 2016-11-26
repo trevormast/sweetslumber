2437592282d13009ccef71f532466fff3ec3fa24
trevormast <trevormast@gmail.com>
2016-11-26 11:18:24 -0800

    Fixes site header
    Addresses #44

--- app/assets/javascripts/tables.js
+++ app/assets/javascripts/tables.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+document.addEventListener('turbolinks:load',function() {
     var table = $('.datatable').DataTable( {
         responsive: true,
         bPaginate: false,
