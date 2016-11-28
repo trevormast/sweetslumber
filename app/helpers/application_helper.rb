303cf3fd87ca713651da485feb3c30178c081bbb
trevormast <trevormast@gmail.com>
2016-11-28 13:54:24 -0800

    Allows guest access to static pages
    Addresses #50

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def admin_present?
     current_user.try(:admin?)
   end
+
+  def registered_user?
+    current_user.present? && current_user.registrations.present?
+  end
 end
