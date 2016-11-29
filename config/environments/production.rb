be81e12623b30a5e2a45a8c3ec8ff71ed80819df
trevormast <trevormast@gmail.com>
2016-11-28 16:33:32 -0800

    Sets up mailer for production
    Addresses #47

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,17 @@ Rails.application.configure do
   config.secret_token = ENV['SECRET_TOKEN']
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
+  config.action_mailer.default_url_options = { host: 'sweetslumber.heroku.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            Rails.application.secrets.GMAIL_USERNAME,
+    password:             Rails.application.secrets.GMAIL_PASSWORD,
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
