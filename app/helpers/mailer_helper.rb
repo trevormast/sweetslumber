9856ac4750dfd2947d2dba4ba0bc1386263d24da
Trevor Mast <trevormast@gmail.com>
2017-01-08 15:48:15 -0800

    fixes sleep guide link

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -5,26 +5,28 @@ module MailerHelper
 
     age_by_workshop_date = (workshop_date - baby_date_of_birth) / 2592000
 
-    return sleep_guide_base_url + age_range(age_by_workshop_date) + sleep_guide_suffix
+    return sleep_guide_base_url + age_range(age_by_workshop_date) + sleep_guide_suffix(age_by_workshop_date)
   end
 
   def sleep_guide_base_url
     return 'https://www.preciouslittlesleep.com/'
   end
 
-  def sleep_guide_suffix
-    return '-baby-sleep-survival-guide/'
+  def sleep_guide_suffix(age)
+    if age.between?(0, 6)
+      return '-baby-sleep-survival-guide/'
+    else
+      return '-baby-sleep-guide/'
+    end
   end
 
   def age_range(age)
-    if age.between?(0, 3)
+    if age.between?(0, 4)
       return 'newborn'
-    elsif age.between?(4, 6)
+    elsif age.between?(4, 7)
       return '3-6-month'
     elsif age.between?(7, 9)
       return '6-9-month'
-    elsif age.between?(10, 12)
-      return '9-12-month'
     else
       return '9-12-month'
     end
