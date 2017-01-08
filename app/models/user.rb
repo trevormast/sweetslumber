e704b441cac661470bada063584e4450f56bd16a
Trevor Mast <trevormast@gmail.com>
2017-01-07 22:42:25 -0800

    Adds scope on user to order by baby dob

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   has_many :registrations, dependent: :destroy
   has_many :workshops, through: :registrations
+
+  scope :sorted_by_baby_dob, -> (workshop = Workshop.all) { joins(registrations: :questionaire)
+                                                            .where(registrations: {workshop_id: workshop})
+                                                            .order('questionaires.baby_dob DESC') }
 end
