c97ca3cf7fdcfc8df1e17aa28cf0de7300a0f220
Trevor Mast <trevormast@gmail.com>
2018-02-17 20:30:51 -0800

    further congifures capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "192.168.1.13", user: "pi", roles: %w{app db web}
+set :ssh_options, { forward_agent: true }
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
